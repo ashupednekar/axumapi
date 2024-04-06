9a63361861d2b919bc2fc92d6c3b4db205ffd5d4
ashupednekar <ashutoshpednekar15@gmail.com>
2024-04-07 03:35:29 +0530

    chore: trying to set up pyo3 async

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 use pyo3::prelude::*;
+use pyo3_asyncio::{tokio, TaskLocals};
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn start_server() -> PyResult<()> {
-    println!("Starting server...");
+fn start_server(py: Python) -> PyResult<()> {
+    let locals = TaskLocals::with_running_loop(py)?.copy_context(py)?;
+    pyo3_asyncio::tokio::future_into_py_with_locals(py, locals.clone(), async move {
+        println!("Starting server...");
+        Ok(())
+    });
     Ok(())
 }
 
