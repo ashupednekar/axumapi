7f9096a2c99afd3cf6f992d584680c8797942d0e
ashupednekar <ashutoshpednekar15@gmail.com>
2024-04-07 17:15:34 +0530

    feat: got server to work

--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,11 @@
-use axum::Router;
 use pyo3::prelude::*;
-use pyo3_asyncio::*;
 
-#[pyfunction]
-pub fn start_server(py: Python) -> PyResult<&PyAny> {
-    // Construct the task locals structure with the current running loop and context
+use ::tokio::net::TcpListener;
+use ::tokio::runtime::Builder;
+use axum::{routing::get, Router};
+use std::future::Future;
+/*fn call_python(py: Python<'static>) -> Result<&PyAny, PyErr> {
     let locals = pyo3_asyncio::TaskLocals::with_running_loop(py)?.copy_context(py)?;
-
     // Convert the async move { } block to a Python awaitable
     pyo3_asyncio::tokio::future_into_py_with_locals(py, locals.clone(), async move {
         let py_sleep = Python::with_gil(|py| {
@@ -15,9 +14,29 @@ pub fn start_server(py: Python) -> PyResult<&PyAny> {
                 py.import("asyncio")?.call_method1("sleep", (1,))?,
             )
         })?;
-
         py_sleep.await?;
-
         Ok(())
     })
+}*/
+
+#[pyfunction]
+pub fn start_server(py: Python) -> PyResult<()> {
+    let rt = Builder::new_multi_thread().enable_all().build().unwrap();
+
+    rt.block_on(async move {
+        serve().await;
+    });
+
+    Ok(())
+}
+
+async fn serve() {
+    let app = Router::new().route("/", get(root));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
