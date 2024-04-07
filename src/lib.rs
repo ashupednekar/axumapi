742f9494279b23033a741bb22184f39d98b476f7
ashupednekar <ashutoshpednekar15@gmail.com>
2024-04-07 22:51:10 +0530

    chore: sync - hiccup: not sure how to pass py to handler for invoking python

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use pyo3::{prelude::*, wrap_pyfunction};
 
 mod server;
+mod handler {
+    pub mod helpers;
+    pub mod router;
+}
 
 #[pymodule]
 fn axumapi(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(server::start_server, m)?)?;
     Ok(())
 }
-
--- src/server.rs
+++ src/server.rs
@@ -1,41 +1,44 @@
 use pyo3::prelude::*;
 
+use super::handler::helpers::call_python;
 use ::tokio::net::TcpListener;
 use ::tokio::runtime::Builder;
-use axum::{routing::get, Router};
-/*fn call_python(py: Python<'static>) -> Result<&PyAny, PyErr> {
+use axum::{routing::get, Extension, Router};
+use std::sync::{Arc, Mutex};
+
+#[pyfunction]
+pub fn start_server(py: Python) -> PyResult<()> {
+    let rt = Builder::new_multi_thread().enable_all().build().unwrap();
+
     let locals = pyo3_asyncio::TaskLocals::with_running_loop(py)?.copy_context(py)?;
     // Convert the async move { } block to a Python awaitable
     pyo3_asyncio::tokio::future_into_py_with_locals(py, locals.clone(), async move {
         let py_sleep = Python::with_gil(|py| {
             pyo3_asyncio::into_future_with_locals(
                 &locals,
-                py.import("asyncio")?.call_method1("sleep", (1,))?,
+                py.import("starbucks.menu")?
+                    .call_method1("list_americano", (1,))?,
             )
         })?;
-        py_sleep.await?;
+        let l = py_sleep.await?;
+        println!("l: {:?}", l);
         Ok(())
-    })
-}*/
-
-#[pyfunction]
-pub fn start_server(py: Python) -> PyResult<()> {
-    let rt = Builder::new_multi_thread().enable_all().build().unwrap();
+    });
 
     rt.block_on(async move {
-        serve().await;
+        serve(py).await;
     });
 
     Ok(())
 }
 
-async fn serve() {
+async fn serve<'a>(py: Python<'a>) {
     let app = Router::new().route("/", get(root));
-
+    println!("Starting server...");
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn root() -> &'static str {
-    "Hello, World!"
+    "hello"
 }
