152b811fba8a2a93819c5c95271f12479da25eab
ashupednekar <ashutoshpednekar15@gmail.com>
2024-04-27 23:46:05 +0530

    feat: returning invoked function response

--- src/handlers/root.rs
+++ src/handlers/root.rs
@@ -5,6 +5,6 @@ pub async fn handle() -> String {
 
     // TODO: add openapi parsing... with module tag support
 
-    let _ = call_python("");
-    "yay".to_owned()
+    let r = call_python("examples/music:songs.py:list");
+    r.unwrap().to_string()
 }
